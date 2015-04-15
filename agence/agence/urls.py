fdf0892f74475f733b28fe77b48d6dc28199027b
Hugo Roussaffa <hugoroussaffa@gmail.com>
2015-04-15 20:43:04 +0700

    version 0.2 page pro

--- agence/agence/urls.py
+++ agence/agence/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from catalogue.views import home
+from django.contrib.auth import views as auth_views
+
 
 
 
@@ -11,6 +13,8 @@ urlpatterns = patterns('',
     url(r'^catalog/', include('catalogue.urls', namespace="catalog")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^photologue/', include('photologue.urls', namespace='photologue')),
+    url(r'^accounts/login/$', auth_views.login),
+    #url('^', include('django.contrib.auth.urls')),
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
